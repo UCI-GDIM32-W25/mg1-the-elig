365cd14a869a3725abcc5e84273e196d2c434890
the-elig <elidg@uci.edu>
2026-01-11 15:16:09 -0800

    movement, planting, and UI functional

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -6,8 +6,9 @@ public class PlantCountUI : MonoBehaviour
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
-    public void UpdateSeeds (int seedsLeft, int seedsPlanted)
+    public void UpdateSeeds (int seedsPlanted, int seedsRemaining)
     {
-        
+        _plantedText.text = seedsPlanted.ToString();
+        _remainingText.text = seedsRemaining.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private float _speed = 3.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
-    [SerializeField] private int _numSeeds = 5; 
+    [SerializeField] private int _numSeeds = 5;
     [SerializeField] private PlantCountUI _plantCountUI;
 
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
-    private void Start ()
+    private void Start()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        _plantCountUI.UpdateSeeds(_numSeedsPlanted, _numSeedsLeft);
     }
 
     private void Update()
     {
-        
+        // movement
+        Vector3 horizontalDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        Vector3 verticalDirection = new Vector3(0, Input.GetAxis("Vertical"), 0);
+
+        transform.Translate(horizontalDirection * _speed * Time.deltaTime);
+        transform.Translate(verticalDirection * _speed * Time.deltaTime);
+
+
+        // triggering planting seeds
+        if (Input.GetKeyDown(KeyCode.Space) && _numSeedsLeft > 0)
+            PlantSeed();
+
+        _plantCountUI.UpdateSeeds(_numSeedsPlanted, _numSeedsLeft);
     }
 
-    public void PlantSeed ()
-    {
-        
+    public void PlantSeed()
+    {   
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        _numSeedsLeft--;
+        _numSeedsPlanted++;        
     }
 }
